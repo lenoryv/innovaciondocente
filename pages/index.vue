64ad5ca775bf2449a559d5108ab92e7ef5cb1c82
Bruno Esparza <bruno.be81@gmail.com>
2018-05-06 23:14:32 -0500

    chore: add nuxt with directories

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    @{{ name }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "baesparza"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.red {
+  color: slategrey;
+}
+</style>
