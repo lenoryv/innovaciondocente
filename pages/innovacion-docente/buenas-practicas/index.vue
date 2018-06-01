ca98eecc9740821833f05e718ffe8e0937c1184c
jonarosero <jars9605@hotmail.com>
2018-06-01 12:52:18 -0500

    fix and new page

--- pages/innovacion-docente/buenas-practicas/index.vue
+++ pages/innovacion-docente/buenas-practicas/index.vue
@@ -1,12 +1,18 @@
 <template>
     <section>
-        <div class="menu">
-        <div id="mySidenav" class="sidenav">
-            <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'1'}}" id="biologia">Biológica</nuxt-link>
-            <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'11'}}" id="tecnica">Técnica</nuxt-link>
-            <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'23'}}" id="social">Sociohumanística</nuxt-link>
-            <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'11'}}" id="admin">Administrativa</nuxt-link>
+        <div class="arrows">
+            <div
+                class="arrow">
+                <span></span>
+            </div>
         </div>
+        <div class="menu">   
+            <div id="mySidenav" class="sidenav">
+                <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'1'}}" id="biologia">Biológica</nuxt-link>
+                <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'11'}}" id="tecnica">Técnica</nuxt-link>
+                <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'23'}}" id="social">Sociohumanística</nuxt-link>
+                <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index-id',params:{id:'11'}}" id="admin">Administrativa</nuxt-link>
+            </div>
         </div>
         <div class="container">
             <nuxt-child/>
@@ -16,46 +22,90 @@
 
 <style lang="scss">
 @import "assets/variables";
-.menu{
-    position: sticky;
+.menu {
+  position: sticky;
 }
 #mySidenav a {
-    position: absolute; 
-    left: -150px; 
-    transition: 0.3s; 
-    padding: 15px; 
-    width: 200px; 
-    text-decoration: none; 
-    font-size: 17px;
-    color: $color-font-primary; 
-    border-radius: 0 5px 5px 0;
-    opacity: 0.85; 
-    
+  position: absolute;
+  left: -150px;
+  transition: 0.3s;
+  padding: 15px;
+  width: 200px;
+  text-decoration: none;
+  font-size: 17px;
+  color: $color-font-primary;
+  border-radius: 0 5px 5px 0;
+  opacity: 0.85;
 }
 
 #mySidenav a:hover {
-    left: 0; 
+  left: 0;
 }
 
 #biologia {
-    top: 80px;
-    background-color: $color-success;
+  top: 80px;
+  background-color: $color-success;
 }
 
 #tecnica {
-    top: 140px;
-    background-color: $color-tecnica; 
+  top: 140px;
+  background-color: $color-tecnica;
 }
 
 #social {
-    top: 200px;
-    background-color: $color-danger;
+  top: 200px;
+  background-color: $color-danger;
 }
 
 #admin {
-    top: 260px;
-    background-color: $color-administrativa 
+  top: 260px;
+  background-color: $color-administrativa;
+}
+.arrow {
+  padding-top: 60px;
+}
+.arrows{
+    position: absolute;
 }
 
+.arrow span {
+  position: absolute;
+  top: 0;
+  width: 24px;
+  height: 24px;
+  margin-left: 10px;
+  border-left: 2px solid $color-primary;
+  border-bottom: 2px solid $color-primary;
+  -webkit-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+  -webkit-animation: sdb05 1.5s infinite;
+  animation: sdb05 1.5s infinite;
+  @-webkit-keyframes sdb05 {
+    0% {
+      -webkit-transform: rotate(-45deg) translate(0, 0);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      -webkit-transform: rotate(-45deg) translate(-20px, 20px);
+      opacity: 0;
+    }
+  }
+  @keyframes sdb05 {
+    0% {
+      transform: rotate(-45deg) translate(0, 0);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      transform: rotate(-45deg) translate(-20px, 20px);
+      opacity: 0;
+    }
+  }
+}
 </style>
 
--- pages/innovacion-docente/proyectos-actuales/index.vue
+++ pages/innovacion-docente/proyectos-actuales/index.vue
@@ -1,60 +1,36 @@
 <template>
-    <section>
-        <div class="container">
-            <h1>Proyectos Actuales</h1>
-            <div class="row">
-                <section>
-                    <div class="embed-container">
-                        <div class="col-lg-4">
-                            <iframe src="https://www.youtube.com/embed/dqRxi9vvIGw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <h3>Clase invertida apoyada en el software NB</h3>
-                        <p>Esta buena práctica consiste en utilizar la clase invertida apoyándose en el programa NB como un recurso para mejorar el proceso de enseñanza-aprendizaje que contempla tres fases: diagnóstico, aplicación de la práctica y verificación de resultados.</p>
-                    </div>
-                </section>
-                <section>
-                    <div class="col-lg-8">
-                        <h3>Partiendo desde la realidad, conozco y actuo- convive con los ríos </h3>
-                        <p>Partiendo desde la realidad, conozco y actuo, es una práctica que se integra dentro del proyecto de vinculación denominado “Convive con los ríos”, abarca 5 comunidades e involucra a estudiantes de Gestión Ambiental y Biología, guiados por tutores para plasmar desde la realidad social, soluciones viables afianzadas en el conocimiento adquirido. La metodología se basa en Flipped Classroom de manera cooperativo como herramienta de aprendizaje para trabajar con comunidades rurales. Los objetivos logrados son la implicación social y el diseño de herramientas estratégicas aplicadas como base para la formulación de propuestas enfocadas al manejo y conservación de los recursos naturales.</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <iframe src="https://www.youtube.com/embed/M8NPO3qEZeQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    </div>
-                </section>
-                <section>
-                    <div class="col-lg-4">
-                        <iframe src="https://www.youtube.com/embed/McFhVxNc8yM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    </div>
-                    <div class="col-lg-8">
-                        <h3> Historia del arte mediante infografía</h3>
-                        <p>Los aspectos positivos del trabajo han demostrado que el aprendizaje mediante la generación de un objeto de diseño comunicacional (infografía), desarrolla en los educandos competencias específicas como la de un incremento en el manejo de lenguaje claro y eficientemente comunicacional para el fin buscado.</p>
-                    </div>
-                </section>
-                <section>
-                    <div class="col-lg-8">
-                        <h3>Aprendizaje basado en objetos: uso de juegos lúdicos como una estrategia didáctica para la enseñanza de la matemática</h3>
-                        <p>El objetivo principal de este trabajo es el uso de juegos matemáticos en el aula para reforzar el aprendizaje de los contenidos teóricos mediante actividades lúdicas. Con el fin de llevar a la práctica esta metodología docente, se desarrollaron juegos lúdicos en aulas a nivel superior durante el semestre Octubre 2017- Febrero 2018, una experiencia basada en el aprendizaje-enseñanza de las matemáticas y el cálculo con alumnos de diferentes titulaciones. Los resultados de las encuestas y promedio de calificaciones demuestran la motivación y el entusiasmo del alumno por el estudio de la matemática mediante esta estrategia.</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <iframe src="https://www.youtube.com/embed/Tqgj5urDopw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </section>
+  <div>
+    <section class="container">
+      <h1>¡Potencia tu formación docente!</h1>
+      <p>
+        Mes a mes, el Plan de Formación Docente Pedagógica ofrece cursos al profesorado de la UTPL para la mejora
+        de su formación académica. Te presentamos los testimonios de los expertos que han visitado el
+        Campus UTPL para trabajar en temáticas que benefician la preparación de los docentes.
+      </p>
+    </section>  
+  </div>
 </template>
 
+<script>
+import proyectos from "@/static/data/proyectos-actuales.json";
+export default {
+    asyncData({ params }) {
+    let proyecto = null;
+    for (let i = 0; i < proyectos.length; i++) {
+              if (proyectos[i] == params.id)
+                proyecto = proyectos[i];
+    }
+    return { proyecto };
+    },
+    validate({ params }) {
+    for (let i = 0; i < proyectos.length; i++)
+          if (proyectos[i] == params.id) return true;
+    return false;
+  }
+};
+</script>
+
 <style lang="scss" scoped>
-.col-lg-4 {
-  margin: 0;
-  padding: 0px;
-}
-.col-lg-8 {
-  margin: 0;
-  padding: 0px;
-}
 .embed-container {
   position: relative;
   padding-bottom: 56.25%;
