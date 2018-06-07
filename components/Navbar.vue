a8f6e49a7465d3c6b2011f6c963440fb816a29c8
Bruno Esparza <bruno.be81@gmail.com>
2018-06-06 23:15:26 -0500

    feat(*): add new navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,4 +1,171 @@
 <template>
+  <header>
+    <nav class="container">
+      <nuxt-link :to="{name: 'index'}"
+                 style="font-weight: 700;">
+        UTPL
+      </nuxt-link>
+      <!------Innovación Docente-------->
+      <div class="dropdown">
+        <nuxt-link :to="{name: 'innovacion-docente'}"
+                   class="dropbtn"
+                   tag="button">
+          Innovación Docente
+          <i class="fa fa-caret-down"></i>
+        </nuxt-link>
+        <div class="dropdown-content">
+          <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index'}">Convocatorias</nuxt-link>
+          <nuxt-link :to="{name: 'innovacion-docente-proyectos-actuales'}">Proyectos Actuales</nuxt-link>
+          <nuxt-link :to="{name: 'innovacion-docente-poyecto-mentores'}">Proyecto Mentores</nuxt-link>
+          <nuxt-link :to="{name: 'innovacion-docente-buenas-practicas-index'}">Buenas Practicas</nuxt-link>
+          <nuxt-link :to="{name: 'formacion-docente-ayudante-catedra'}">Ayudante de Catedra</nuxt-link>
+          <a target="_blank"
+             rel="noopener"
+             href="https://retos.utpl.edu.ec/">Retos</a>
+        </div>
+      </div>
+      <!------Formación Docente-------->
+      <div class="dropdown">
+        <nuxt-link :to="{name: 'formacion-docente-programa-formacion'}"
+                   class="dropbtn"
+                   tag="button">
+          Formación Docente
+          <i class="fa fa-caret-down"></i>
+        </nuxt-link>
+        <div class="dropdown-content">
+          <nuxt-link :to="{name: 'formacion-docente-programa-formacion'}">Programa de Formación</nuxt-link>
+          <nuxt-link :to="{name: 'formacion-docente-cafe-cientifico'}">Café Científico</nuxt-link>
+          <nuxt-link :to="{name: 'formacion-docente-jornadas-de-reflexion'}">Jornadas de Reflexión</nuxt-link>
+        </div>
+      </div>
+      <!------Observatorio EduTencencias-------->
+      <nuxt-link :to="{name: 'observatorio-edutendencias'}">
+        Observatorio EduTencencias
+      </nuxt-link>
+      <a href="javascript:void(0);"
+         style="font-size:15px;"
+         class="icon"
+         onclick="myFunction()">&#9776;</a>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+@import "assets/variables";
+///////options/////////
+$padding: 12px;
+
+//  @supports ((position: -webkit-sticky) or (position: sticky)) {
+//    header {
+//      position: -webkit-sticky;
+//      position: sticky;
+//      top: 0;
+//      z-index: 1020;
+//    }
+//  }
+
+header {
+  overflow: hidden;
+  background-color: $color-primary;
+  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.7);
+  a {
+    float: left;
+    display: block;
+    color: $color-font-primary;
+    text-align: center;
+    padding: $padding;
+    text-decoration: none;
+  }
+  .icon {
+    display: none;
+  }
+}
+
+.nuxt-link-exact-active {
+  font-weight: 500;
+  font-style: italic;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+  .dropbtn {
+    border: none;
+    outline: none;
+    color: $color-font-primary;
+    padding: $padding;
+    background-color: inherit;
+    margin: 0;
+    cursor: pointer;
+  }
+  &-content {
+    display: none;
+    position: absolute;
+    background-color: $color-font-primary;
+    box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.7);
+    min-width: 100px;
+    z-index: 1020;
+    a {
+      float: none;
+      background-color: $color-font-primary;
+      color: $color-primary;
+      padding: $padding;
+      text-decoration: none;
+      display: block;
+      text-align: left;
+      &:hover {
+        background-color: $color-primary;
+        color: $color-font-primary;
+      }
+    }
+  }
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+@media screen and (max-width: 600px) {
+  header {
+    a:not(:first-child),
+    .dropdown .dropbtn {
+      display: none;
+    }
+    a.icon {
+      float: right;
+      display: block;
+    }
+  }
+}
+
+@media screen and (max-width: 600px) {
+  header.responsive {
+    position: relative;
+    .icon {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+    a {
+      float: none;
+      display: block;
+      text-align: left;
+    }
+    .dropdown-content {
+      position: relative;
+    }
+    .dropdown .dropbtn {
+      display: block;
+      width: 100%;
+      text-align: left;
+    }
+  }
+}
+</style>
+
+<!--
+<template>
   <div>
     <div @click="toggleMenu"
          class="menu-icon"
@@ -71,7 +238,6 @@ export default {
   }
 };
 </script>
-
 
 <style lang="scss" scoped >
 @import "assets/variables";
@@ -233,3 +399,4 @@ export default {
   }
 }
 </style>
+-->
