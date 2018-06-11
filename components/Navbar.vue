3486d0a77791ba34f7fb428a97fc6828bf55df94
jonarosero <jars9605@hotmail.com>
2018-06-11 12:47:18 -0500

    update buenas-practicas.png and nuxt-link of navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -19,9 +19,9 @@
             <ul>
               <nuxt-link :to="{name: 'innovacion-docente-ayudante-catedra'}">Ayudante de Catedra</nuxt-link>
               <nuxt-link :to="{name: 'innovacion-docente-convocatorias-index'}">Convocatorias</nuxt-link>
+              <nuxt-link :to="{name: 'innovacion-docente-buenas-practicas-index'}">Portafolio de Buenas Prácticas</nuxt-link>
               <nuxt-link :to="{name: 'innovacion-docente-proyectos-actuales'}">Proyectos Actuales</nuxt-link>
               <nuxt-link :to="{name: 'innovacion-docente-proyecto-mentores'}">Proyecto Mentores</nuxt-link>
-              <nuxt-link :to="{name: 'innovacion-docente-buenas-practicas-index'}">Repositorio de Buenas Prácticas</nuxt-link>
               <a target="_blank"
                  rel="noopener"
                  href="https://retos.utpl.edu.ec/">Retos</a>
