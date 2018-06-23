687000bc26b0866310435ca46864aaba4f46b604
Bruno Esparza <bruno.be81@gmail.com>
2018-06-23 00:27:25 -0500

    feat(cafe): add style

--- pages/formacion-docente/cafe-cientifico/index.vue
+++ pages/formacion-docente/cafe-cientifico/index.vue
@@ -1,40 +1,43 @@
 <template>
   <div>
-    <img v-lazy="banner"
-         alt="banner-cafe-cientifico">
-    <section v-if="ultimoEncuentro">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <figure :style="'background-image: url('+ultimoEncuentro.data.img+');'"></figure>
-            <h3>
-              <i class="fas fa-users"></i> Invitados
-            </h3>
-            <ul>
-              <li v-for="(invitado, index) in ultimoEncuentro.data.invitados"
-                  :key="index">{{ invitado.nombre }}</li>
-            </ul>
-          </div>
-          <div class="col-md-8">
-            <h1>{{ultimoEncuentro.data.nombre}}</h1>
-            <small>{{ultimoEncuentro.data.fecha}}</small>
-            <p>{{ultimoEncuentro.data.contenido | slice(0,500)}}</p>
-            <nuxt-link class="btn btn-primary"
-                       :to="{name: 'formacion-docente-cafe-cientifico-id', params: {id: ultimoEncuentro.key}}">
-              Leer más
-            </nuxt-link>
+    <header v-if="ultimoEncuentro"
+            class="parallax"
+            :style="'background-image: url('+ultimoEncuentro.data.img+');'">
+      <div class="header-overlay"></div>
+      <div class="header-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <figure :style="'background-image: url('+ultimoEncuentro.data.img+');'"></figure>
+              <h3>
+                <i class="fas fa-users"></i> Invitados
+              </h3>
+              <ul>
+                <li v-for="(invitado, index) in ultimoEncuentro.data.invitados"
+                    :key="index">{{ invitado.nombre }}</li>
+              </ul>
+            </div>
+            <div class="col-md-8">
+              <h1>{{ultimoEncuentro.data.nombre}}</h1>
+              <small>{{ultimoEncuentro.data.fecha}}</small>
+              <p>{{ultimoEncuentro.data.contenido | slice(0,500)}}</p>
+              <nuxt-link class="btn btn-primary"
+                         :to="{name: 'formacion-docente-cafe-cientifico-id', params: {id: ultimoEncuentro.key}}">
+                Leer más
+              </nuxt-link>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <section id="portafolio"
-             v-if="encuentros">
+    </header>
+    <section v-if="encuentros"
+             id="encuentros">
       <div class="container-fluid">
         <h2>
           Portafolio de Encuentros
         </h2>
-        <div class="row">
-          <nuxt-link class="col-lg-3 col-sm-6"
+        <div class="row encuentros">
+          <nuxt-link class="col-lg-3 col-md-5 col-sm-7"
                      v-for="(encuentro, key) in encuentros"
                      :key="key"
                      :to="{name: 'formacion-docente-cafe-cientifico-id', params: {id: key}}"
@@ -118,19 +121,41 @@ export default {
 <style lang="scss" scoped>
 @import "assets/variables";
 @import "assets/card";
+@import "assets/parallax";
+
+header {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+}
+
+.header-overlay {
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  background: $color-primary;
+  opacity: 0.6;
+}
+
+.header-content {
+  text-align: center;
+  margin: auto;
+  color: $color-font-primary !important;
+  h1 {
+    color: $color-font-primary !important
+  }
+}
 
 #suscripcion {
   background-color: $color-primary;
   color: $color-font-primary;
 }
-
-#portafolio {
-  color: $color-primary;
-  background-color: $color-warning;
-  .card {
-    padding-bottom: 0;
-    color: $color-primary;
-    background-color: $color-warning;
+#encuentros {
+  overflow: auto;
+  .row {
+    display: -webkit-box;
+    flex-wrap: nowrap;
   }
 }
 
