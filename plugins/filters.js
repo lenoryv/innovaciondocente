a87c7c900679ec2df0b4b9afcfd140716511f4e1
Bruno Esparza <bruno.be81@gmail.com>
2018-06-26 15:48:29 -0500

    feat(date): get date in spanish

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,9 +3,27 @@ import Vue from 'vue'
 Vue.filter('capitalize', val => val.toUpperCase());
 Vue.filter('slice', (val, start, end) => val.slice(start, end) + '...');
 Vue.filter('date', val => {
-  var parts = val.split('-');
-  // Please pay attention to the month (parts[1]); JavaScript counts months from 0:
-  // January - 0, February - 1, etc.
-  var mydate = new Date(parts[0], parts[1] - 1, parts[2]);
-  return mydate.toDateString();
+  let parts = val.split('-');
+
+  let mes;
+
+  console.log(typeof (parts[1]));
+
+  switch (parseInt(parts[1])) {
+    case 1: { mes = "Enero"; break; }
+    case 2: { mes = "Febrero"; break; }
+    case 3: { mes = "Marzo"; break; }
+    case 4: { mes = "Abril"; break; }
+    case 5: { mes = "Mayo"; break; }
+    case 6: { mes = "Junio"; break; }
+    case 7: { mes = "Julio"; break; }
+    case 8: { mes = "Agosto"; break; }
+    case 9: { mes = "Septiembre"; break; }
+    case 10: { mes = "Octubre"; break; }
+    case 11: { mes = "Noviembre"; break; }
+    case 12: { mes = "Diciembre"; break; }
+  }
+  return `${parts[2]} de ${mes}, ${parts[0]}`
 });
+
+
