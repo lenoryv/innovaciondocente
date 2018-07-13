86046c8d43d3596af10ac9b6d639e4dc076f136d
Bruno Esparza <bruno.be81@gmail.com>
2018-07-13 13:33:22 -0500

    feat(jornads): add valoraciones

--- pages/formacion-docente/jornadas-de-reflexion/index.vue
+++ pages/formacion-docente/jornadas-de-reflexion/index.vue
@@ -2,30 +2,55 @@
   <section class="container">
     <div class="row">
       <div class="col-lg-3">
-        <FormaJornada></FormaJornada>
+        <FormaJornada v-bind:LinkKey="encuentro.key"></FormaJornada>
         <nuxt-link :to="{name: 'formacion-docente-jornadas-de-reflexion-jornadas-anteriores'}"
                    class="btn btn-large btn-outline-primary btn-sm">
           <i class="fas fa-folder"></i> Portafolio <br> de Jornadas
         </nuxt-link>
       </div>
       <div class="col-lg-8">
-        <div v-html="jornada.html"></div>
+        <h1>{{encuentro.data.nombre}}</h1>
+        <small v-if="encuentro.data.autor">
+          <i class="fas fa-user"></i> {{encuentro.data.autor}}
+        </small>
+        <small v-if="encuentro.data.date">
+          <br>
+          <i class="fas fa-calendar-alt"></i> {{encuentro.data.date | date}}
+        </small>
+        <hr>
+        <div v-html="encuentro.data.html"
+             class="html"></div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import jornadas from "@/static/data/jornadas.json";
+import axios from "axios";
 import FormaJornada from "@/components/FormaJornada";
 export default {
-  asyncData({ params }) {
-    let jornada = jornadas[0];
-    return { jornada };
+  async asyncData() {
+    let { data } = await axios.get(
+      "https://innovaciondocente-utpl.firebaseio.com/formacion-docente/jornadas-de-reflexion.json?orderBy=%22$key%22&limitToLast=1"
+    );
+
+    let encuentro = null;
+    for (const key in data) {
+      encuentro = { key: key, data: data[key] };
+    }
+
+    return { encuentro };
   },
   head() {
     return {
-      title: "Jornadas de Reflexión | Innovación Docente"
+      title: "Jornadas de Reflexión | Innovación Docente",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.encuentro.description
+        }
+      ]
     };
   },
   components: {
@@ -34,3 +59,6 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+@import "assets/html";
+</style>
