5c2f5a89003a589201869c928fb6047c3d59b464
Bruno Esparza <bruno.be81@gmail.com>
2018-07-20 17:14:08 -0500

    feat(*): final commit

--- pages/observatorio-edutendencias/tips-innovacion/tips.vue
+++ pages/observatorio-edutendencias/tips-innovacion/tips.vue
@@ -3,21 +3,22 @@
     <header>
       <h1>{{title}}</h1>
     </header>
-    <section class="container">
+    <section class="container-fluid">
       <div class="row"
            v-if="cards.length > 0">
         <div v-for="card in cards"
              :key="card.key"
-             class="col-md-3 col-ms-2">
+             class="col-md-4 col-ms-6">
           <figure class="card6">
             <img :src="card.img"
                  :alt="'img' + card.key" />
             <figcaption>
-              <h3>{{card.title|slice(0,40)}}</h3>
-              <p>{{card.description|slice(0,80)}}</p>
+              <h3>{{card.title}}</h3>
+              <p>
+                <a :href="card.url"
+                   target="_blank"></a>
+              </p>
             </figcaption>
-            <a :href="card.url"
-               target="_blank"></a>
           </figure>
         </div>
       </div>
