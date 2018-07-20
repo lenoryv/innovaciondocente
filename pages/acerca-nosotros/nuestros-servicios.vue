4524c72be1647246244972831886ef7baf125ef4
jahurtadod <jahurtadod@outlook.com>
2018-07-20 13:04:21 -0500

    modify "nuestros-servicios"

--- pages/acerca-nosotros/nuestros-servicios.vue
+++ pages/acerca-nosotros/nuestros-servicios.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-         <img src="~/static/servicios.jpg" alt="">
+         <img src="https://firebasestorage.googleapis.com/v0/b/innovaciondocente-utpl.appspot.com/o/acerca-nosotros%2Fservicios.jpg?alt=media&token=c18db257-471b-4ba5-a91a-f8a3a6ae1a1b" alt="">
     </div>
 </template>
 
